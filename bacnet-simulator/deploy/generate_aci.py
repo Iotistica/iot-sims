132e4d62ff50a7bd72d378234c5212b749d42e00
dsamborschi <dsamborschi@gmail.com>
2026-07-01 02:18:34 -0400

    udp

---
+++ bacnet-simulator/deploy/generate_aci.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Generates aci-deploy.json for az container create --file."""
+import json, os, sys
+
+required = ["ACI_NAME", "ACI_DNS_LABEL", "IMAGE", "STORAGE_KEY"]
+missing = [k for k in required if not os.environ.get(k)]
+if missing:
+    print(f"Missing env vars: {missing}", file=sys.stderr)
+    sys.exit(1)
+
+config = {
+    "apiVersion": "2021-10-01",
+    "name": os.environ["ACI_NAME"],
+    "location": "canadacentral",
+    "properties": {
+        "containers": [{
+            "name": "bacnet-sim",
+            "properties": {
+                "image": os.environ["IMAGE"],
+                "resources": {"requests": {"cpu": 1, "memoryInGB": 1.5}},
+                "ports": [
+                    {"protocol": "TCP", "port": 47900},
+                    {"protocol": "UDP", "port": 47808},
+                ],
+                "environmentVariables": [
+                    {"name": "DATA_DIR",     "value": "/data"},
+                    {"name": "SIM_API_PORT", "value": "47900"},
+                    {"name": "BACNET_PORT",  "value": "47808"},
+                ],
+                "volumeMounts": [{"name": "bacnet-data", "mountPath": "/data"}],
+            },
+        }],
+        "volumes": [{
+            "name": "bacnet-data",
+            "azureFile": {
+                "shareName": "bacnet-storage-volume",
+                "storageAccountName": "iotistic",
+                "storageAccountKey": os.environ["STORAGE_KEY"],
+            },
+        }],
+        "ipAddress": {
+            "type": "Public",
+            "dnsNameLabel": os.environ["ACI_DNS_LABEL"],
+            "ports": [
+                {"protocol": "TCP", "port": 47900},
+                {"protocol": "UDP", "port": 47808},
+            ],
+        },
+        "osType": "Linux",
+        "restartPolicy": "Always",
+    },
+}
+
+out = os.environ.get("OUTPUT", "aci-deploy.json")
+with open(out, "w") as f:
+    json.dump(config, f, indent=2)
+print(f"Written {out}")
